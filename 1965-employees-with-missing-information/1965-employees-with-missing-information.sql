7f14d4fd728fcfa58f46949b79b0b7ad3d360a68
Rakesh Kannepelli <167434191+rakeshkannepelli@users.noreply.github.com>
2026-09-05 04:04:14 -0700

    Time: 520 ms (96.21%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-(SELECT employee_id FROM Employees EXCEPT SELECT employee_id FROM Salaries)
+SELECT Employees.employee_id FROM Employees 
+LEFT JOIN Salaries ON Salaries.employee_id = Employees.employee_id
+WHERE Salaries.employee_id IS NULL
 UNION
-(SELECT employee_id FROM Salaries EXCEPT SELECT employee_id FROM Employees)
+SELECT Salaries.employee_id FROM Salaries
+LEFT JOIN Employees ON Employees.employee_id = Salaries.employee_id
+WHERE Employees.employee_id IS NULL
 ORDER BY employee_id ASC;
